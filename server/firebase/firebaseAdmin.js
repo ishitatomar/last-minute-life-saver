2a86c9d577ff9915fe22637dacb09b7e602e7aef
Ishita Tomar <ishitatomar1992@gmail.com>
2026-06-29 01:35:56 +0530

    fix firebase admin env config

--- server/firebase/firebaseAdmin.js
+++ server/firebase/firebaseAdmin.js
@@ -2,12 +2,14 @@ import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getAuth } from "firebase-admin/auth";
 
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+};
+
 initializeApp({
-  credential: cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  }),
+  credential: cert(serviceAccount),
 });
 
 export const db = getFirestore();
