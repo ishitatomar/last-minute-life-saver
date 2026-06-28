788628188fe4801f025b9a31b36df4e11daf8f18
Ishita Tomar <ishitatomar1992@gmail.com>
2026-06-29 02:26:57 +0530

    fix cloud run deploy

--- server/server.js
+++ server/server.js
@@ -1,25 +1,31 @@
-console.log("🔥 APP STARTING...");
-
-process.on("uncaughtException", (err) => {
-  console.error("❌ UNCAUGHT ERROR:", err);
-});
-
-process.on("unhandledRejection", (err) => {
-  console.error("❌ PROMISE ERROR:", err);
-});
+console.log("App starting...");
+import dotenv from "dotenv";
+dotenv.config();
 
 import express from "express";
+import cors from "cors";
+
+//import "./firebase/firebaseAdmin.js";
+import aiRoutes from "./routes/aiRoutes.js";
+
+console.log(
+  "OpenRouter Key:",
+  process.env.OPENROUTER_API_KEY ? "Loaded ✅" : "Missing ❌"
+);
 
 const app = express();
 
-console.log("✔ Express loaded");
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("WORKING OK");
+  res.send("🚀 Last Minute Life Saver Backend Running");
 });
+
+app.use("/api", aiRoutes);
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("🚀 Server running on", PORT);
+app.listen(PORT, () => {
+  console.log("Server running on", PORT);
 });
