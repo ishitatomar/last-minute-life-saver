327bf77cfe045eea36f219dbd41eeb3fdbd3c585
Ishita Tomar <ishitatomar1992@gmail.com>
2026-06-30 03:12:24 +0530

    firebase.js changed

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,10 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+console.log("API KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
+console.log("PROJECT:", import.meta.env.VITE_FIREBASE_PROJECT_ID);
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
@@ -13,6 +18,3 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-console.log("ENV CHECK:", import.meta.env);
-console.log("FIREBASE CONFIG:", firebaseConfig);
