087c6eda23e9c3fa271d268733af6edc797bdbbf
Ishita Tomar <ishitatomar1992@gmail.com>
2026-06-30 03:37:12 +0530

    Fix backend routes

--- server/server.js
+++ server/server.js
@@ -4,12 +4,16 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
+const aiRoutes = require("./routes/aiRoutes");
+
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/", aiRoutes);
 
 app.get("/", (req, res) => {
   res.send("Backend Running");
