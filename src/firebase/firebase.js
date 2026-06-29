868c704ae08df928a89900bfb8484e833d3e5329
Ishita Tomar <ishitatomar1992@gmail.com>
2026-06-30 01:58:44 +0530

    fix firebase + env config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -11,7 +11,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-// 🔥 EXPORT THESE (IMPORTANT)
-export const auth = getAuth(app);
 export const db = getFirestore(app);
-export default app;
+export const auth = getAuth(app);
